184318e59ca4dd3996c6bc01c3e0e624c29ec319
Michael Mroz <michael@mroz.io>
2023-12-24 23:30:26 -0800

    factored log to op-level

--- src/Control/SimpleStateMachine.hs
+++ src/Control/SimpleStateMachine.hs
@@ -1,48 +1,59 @@
 module Control.SimpleStateMachine (
   StateMachine (..),
   MachineData (..),
-  runInit,
-  runTransition,
+  initBlind,
+  transitionBlind,
 ) where
 
+import Data.Functor ((<&>))
 import Data.Kind (Type)
 
 data MachineData machineTag stateTag where
   MachineData :: {props :: Props machineTag, state :: StateData machineTag stateTag} -> MachineData machineTag stateTag
 
-class (Monad m) => StateMachine m (machineTag :: Type) (stateTag :: Type) (yield :: Type) | machineTag -> stateTag yield where
+class (Monad m) => StateMachine m (machineTag :: Type) (stateTag :: Type) | machineTag -> stateTag where
   data Props machineTag :: Type
   data StateData machineTag :: stateTag -> Type
-  data Transition machineTag :: stateTag -> stateTag -> Type
-  data Init machineTag :: stateTag -> Type
+  data Init machineTag :: stateTag -> Type -> Type
+  data Transition machineTag :: stateTag -> stateTag -> Type -> Type
 
   -- | Initialize a state machine, returning the initial @MachineData@ and a yield.
   initialize ::
-    forall (s0 :: stateTag).
-    Init machineTag s0 ->
+    forall (s0 :: stateTag) yield.
+    Init machineTag s0 yield ->
     m (MachineData machineTag s0, yield)
 
   -- | Run a transition on a state machine, returning the new @StateData@ and a yield.
   --   This is intended to be internal-only. Consumers should use @runTransition@.
-  transition ::
-    forall (s1 :: stateTag) (s2 :: stateTag).
-    Transition machineTag s1 s2 ->
+  transitionState ::
+    forall (s1 :: stateTag) (s2 :: stateTag) yield.
+    Transition machineTag s1 s2 yield ->
     MachineData machineTag s1 ->
     m (StateData machineTag s2, yield)
 
+transition ::
+  forall (m :: Type -> Type) (machineTag :: Type) (st :: Type) (yield :: Type) (s1 :: st) (s2 :: st).
+  (StateMachine m machineTag st) =>
+  Transition machineTag s1 s2 yield ->
+  MachineData machineTag s1 ->
+  m (MachineData machineTag s2, yield)
+transition t d = transform <$> transitionState t d
+ where
+  transform (newState, yield) = (MachineData d.props newState, yield)
+
 -- | Initialize a state machine. This should actually act on the yield, but for now we'll discard it.
-runInit ::
+initBlind ::
   forall (m :: Type -> Type) (machineTag :: Type) (stateTag :: Type) (yield :: Type) (s0 :: stateTag).
-  (StateMachine m machineTag stateTag yield) =>
-  Init machineTag s0 ->
+  (StateMachine m machineTag stateTag) =>
+  Init machineTag s0 yield ->
   m (MachineData machineTag s0)
-runInit i = fst <$> initialize i
+initBlind i = fst <$> initialize i
 
 -- | Run a transition on a state machine. This should actually act on the yield, but for now we'll discard it.
-runTransition ::
+transitionBlind ::
   forall (m :: Type -> Type) (machineTag :: Type) (st :: Type) (yield :: Type) (s1 :: st) (s2 :: st).
-  (StateMachine m machineTag st yield) =>
-  Transition machineTag s1 s2 ->
+  (StateMachine m machineTag st) =>
+  Transition machineTag s1 s2 yield ->
   MachineData machineTag s1 ->
   m (MachineData machineTag s2)
-runTransition t d = MachineData d.props . fst <$> transition t d
+transitionBlind t d = transition t d <&> fst
--- src/Examples/MutualFunds/Buy.hs
+++ src/Examples/MutualFunds/Buy.hs
@@ -21,7 +21,7 @@ data MutualFundPurchase = MutualFundPurchase deriving (Eq, Show)
 
 data MutualFundPurchaseState = Pending | Listed | Closed | Failed deriving (Eq, Show)
 
-instance (Monad m) => StateMachine m MutualFundPurchase MutualFundPurchaseState LogYield where
+instance (Monad m) => StateMachine m MutualFundPurchase MutualFundPurchaseState where
   data Props MutualFundPurchase = Props
     { fund :: MutualFund
     , projectedUnits :: ShareUnits
@@ -34,25 +34,25 @@ instance (Monad m) => StateMachine m MutualFundPurchase MutualFundPurchaseState 
     ClosedData :: StateData MutualFundPurchase 'Closed
     FailedData :: StateData MutualFundPurchase 'Failed
 
-  data Transition MutualFundPurchase :: MutualFundPurchaseState -> MutualFundPurchaseState -> Type where
-    List :: OrderId -> Transition MutualFundPurchase 'Pending 'Listed
-    Close :: Dollars -> ShareUnits -> Transition MutualFundPurchase 'Listed 'Closed
-    Fail :: Transition MutualFundPurchase 'Pending 'Failed
+  data Transition MutualFundPurchase :: MutualFundPurchaseState -> MutualFundPurchaseState -> Type -> Type where
+    List :: OrderId -> Transition MutualFundPurchase 'Pending 'Listed LogYield
+    Close :: Dollars -> ShareUnits -> Transition MutualFundPurchase 'Listed 'Closed LogYield
+    Fail :: Transition MutualFundPurchase 'Pending 'Failed LogYield
 
-  data Init MutualFundPurchase :: MutualFundPurchaseState -> Type where
-    InitPending :: Props MutualFundPurchase -> Init MutualFundPurchase 'Pending
+  data Init MutualFundPurchase :: MutualFundPurchaseState -> Type -> Type where
+    InitPending :: Props MutualFundPurchase -> Init MutualFundPurchase 'Pending LogYield
 
   initialize ::
-    forall (s0 :: MutualFundPurchaseState).
-    Init MutualFundPurchase s0 ->
-    m (MachineData MutualFundPurchase s0, LogYield)
+    forall (s0 :: MutualFundPurchaseState) yield.
+    Init MutualFundPurchase s0 yield ->
+    m (MachineData MutualFundPurchase s0, yield)
   initialize (InitPending props) = pure (MachineData props PendingData, LogYield "Initialized with Pending State")
 
-  transition ::
-    forall (s1 :: MutualFundPurchaseState) (s2 :: MutualFundPurchaseState).
-    Transition MutualFundPurchase s1 s2 ->
+  transitionState ::
+    forall (s1 :: MutualFundPurchaseState) (s2 :: MutualFundPurchaseState) yield.
+    Transition MutualFundPurchase s1 s2 yield ->
     MachineData MutualFundPurchase s1 ->
-    m (StateData MutualFundPurchase s2, LogYield)
-  transition (List orderId) _ = pure (ListedData orderId, LogYield "Listed")
-  transition (Close dollars units) _ = pure (ClosedData, LogYield ("Closed (" <> show units <> " <-> $" <> show dollars <> ")"))
-  transition Fail _ = pure (FailedData, LogYield "Failed")
+    m (StateData MutualFundPurchase s2, yield)
+  transitionState (List orderId) _ = pure (ListedData orderId, LogYield "Listed")
+  transitionState (Close dollars units) _ = pure (ClosedData, LogYield ("Closed (" <> show units <> " <-> $" <> show dollars <> ")"))
+  transitionState Fail _ = pure (FailedData, LogYield "Failed")
